f9fcc6d348e06abf75e8b81d1d252a1caef5ad24
Chirag Parmar <chiragparmar12177@gmail.com>
2020-01-26 13:16:10 +0530

    define BOARD_SEPOLICY_VERS := 29.0
    Signed-off-by: Chirag Parmar <chiragparmar12177@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_VERS := 29.0
 
 # Treble
 BOARD_VNDK_RUNTIME_DISABLE := true
